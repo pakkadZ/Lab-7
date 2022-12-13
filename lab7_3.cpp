173c8520a2f4e41b9f6a5e4c836d011af9861ce6
Pongsapak <pakkdjor00@gmail.com>
2022-12-13 17:24:23 +0700

    lab 7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,32 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
 
+
+int adiff(int x,int y){
+  int a,b;
+  int z=abs(y-x);
+  a = z%360;
+  if(a<=180){
+    b=a;
+  }
+  else{
+    b=360-a;
+  }
+
+return b;
+
+}
+
+
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<<"\n";
+  cout << adiff(210,45)<<"\n";
+  cout << adiff(0,360)<<"\n";
+  cout << adiff(10,350)<<"\n";
+  cout << adiff(95,260)<<"\n";
+  cout << adiff(90,-90)<<"\n";
+  cout << adiff(1000,280)<<"\n";
+  cout << adiff(60,244)<<"\n";
 }
